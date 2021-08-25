c195b01bbfcc3f211429895e716c59f310a6291c
tecgym <tecgymcolombiana@gmail.com>
2021-08-24 19:44:26 -0500

    commit de primer codigo en mplab

---
+++ prueba.X/main.c
@@ -0,0 +1,16 @@
+/*
+ * File:   main.c
+ * Author: Felipe Cantillo
+ *
+ * Created on August 24, 2021, 7:41 PM
+ */
+
+
+#include <xc.h>
+
+void main(void) {
+    
+    primer codigo
+            
+    return;
+}
